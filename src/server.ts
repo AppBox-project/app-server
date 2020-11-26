5c39c06e8d2fa42c6b814837ebf3cb670bbc08c5
Vic van Cooten <duveaux1@gmail.com>
2020-11-26 15:07:22 +0100

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+//@ts-ignore
+var mongoose = require("mongoose");
+var vhost = require("vhost");
+const express = require("express");
+const app = express();
+const port = 8601;
+
+// Models
+require("./Utils/Models/SystemSettings");
+
+mongoose.connect(`mongodb://${process.env.DBURL || "localhost:27017"}/AppBox`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+var db = mongoose.connection;
+db.once("open", async () => {
+  // Models
+  const systemsettings = mongoose.model("SystemSettings");
+
+  const apps = await systemsettings.findOne({ key: "hosted_apps" });
+  apps.value.map((app) => {
+    console.log(app);
+  });
+
+  app.listen(port, () => {
+    console.log(
+      `App-Server is now listening for VHOST requests on http://localhost:${port}`
+    );
+  });
+
+  console.log("App-Server initialising.");
+});
